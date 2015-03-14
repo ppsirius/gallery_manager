4b2602400c9a5a8230bdae5c4d57d91a8a541749
Piotr Pupczyk <piotrek@jakbyco.com>
2015-03-14 23:23:17 +0100

    add some validations

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,9 +1,22 @@
 class Image < ActiveRecord::Base
+  validates :name, :presence => true
+  validates :description, :uniqueness => true
   has_attached_file :photo,  
                     :styles => { :large => '1920x1200', :medium => "800x600>", :thumb => "200x200>" },
                     :unless => Proc.new { |imports| imports.photo_file_name.blank? }
   validates_attachment_content_type :photo, :content_type => ["image/jpg", "image/jpeg"]
+  validates_attachment_size :photo, :less_than => 2.megabytes
+
+  before_create :set_visible
+
+  def set_visible
+    self.visible = true
+  end
+
 end
+
+
+
 
 
 
