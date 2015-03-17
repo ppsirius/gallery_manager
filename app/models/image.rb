8e36ec67390d11dd53007d336c783cab82d5f18e
Piotr Pupczyk <piotrek@jakbyco.com>
2015-03-17 23:15:14 +0100

    Added rails_12factor gem and updated Gemfile.lock

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,8 +1,8 @@
 class Image < ActiveRecord::Base
-  validates :name, :presence => true
+  validates :name, :photo, :presence => true
   validates :description, :uniqueness => true
   has_attached_file :photo,  
-                    :styles => { :large => '1920x1200', :medium => "800x600>", :thumb => "200x200>" },
+                    :styles => { :large => '1920x1200', :medium => "800x600>", :thumb => "300x300>" },
                     :unless => Proc.new { |imports| imports.photo_file_name.blank? }
   validates_attachment_content_type :photo, :content_type => ["image/jpg", "image/jpeg"]
   validates_attachment_size :photo, :less_than => 2.megabytes
