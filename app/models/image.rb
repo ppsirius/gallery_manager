b94274ff6d2d51a85d19db15e131b14f3e953aba
Piotr Pupczyk <piotrek@jakbyco.com>
2015-04-02 18:12:36 +0200

    add comma

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,7 +2,7 @@ class Image < ActiveRecord::Base
   validates :name, :photo, :presence => true
   validates :description, :uniqueness => true
   has_attached_file :photo,  
-                    :styles => { :large => '1920x1200', :medium => "800x600>", :thumb => "300x300>" }
+                    :styles => { :large => '1920x1200', :medium => "800x600>", :thumb => "300x300>" },
                     :path => "/image/:id/:style/:filename"
   validates_attachment_content_type :photo, :content_type => ["image/jpg", "image/jpeg"]
   validates_attachment_size :photo, :less_than => 2.megabytes
